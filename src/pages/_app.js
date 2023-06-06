03b125274ffd42ea52d4372b551834d87e85a5a9
John Barrier Wilson <johnbarrierwilson@gmail.com>
2023-06-06 16:59:16 -0500

    Add next/font and space grotesk

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import { Space_Grotesk } from "@next/font/google";
+
+import "@/styles/globals.css";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${spaceGrotesk.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
