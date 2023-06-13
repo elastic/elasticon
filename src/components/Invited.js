ef51fd1e53b83d4ffec880442a710c5fbb0f592f
John Barrier Wilson <johnbarrierwilson@gmail.com>
2023-06-13 15:20:11 -0500

    Add stars to invited heading

--- src/components/Invited.js
+++ src/components/Invited.js
@@ -2,11 +2,36 @@ import Heading from "@/components/Heading";
 import Wave from "@/components/Wave";
 
 export default function Invited() {
+  const headingAfterClasses = `
+    after:absolute
+    after:bg-[url('/images/icon-star.svg')]
+    after:content-[' ']
+    after:h-6
+    after:-right-14
+    after:top-1
+    after:w-6
+  `;
+
+  const headingBeforeClasses = `
+    before:absolute
+    before:bg-[url('/images/icon-star.svg')]
+    before:content-[' ']
+    before:h-6
+    before:-left-14
+    before:top-1
+    before:w-6
+  `;
+
   return (
     <div className="border-2 border-dashed border-zinc-400 flex flex-col md:flex-row items-center mb-4 p-9 rounded-md md:rounded-full">
       <Wave />
       <div className="max-w-4xl mx-auto md:px-8 text-zinc-900 text-center">
-        <Heading size="h4">You&apos;re invited!</Heading>
+        <Heading
+          className={`inline-block relative ${headingAfterClasses} ${headingBeforeClasses}`}
+          size="h4"
+        >
+          You&apos;re invited!
+        </Heading>
         <p className="mt-3 text-lg">
           The entire Elastic community is welcome: Developers, Architects, IT
           professionals, DevOps Engineers, and anyone working with or interested
