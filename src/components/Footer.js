9795ed1407d5875062979d035dd80ec5bc254591
John Barrier Wilson <johnbarrierwilson@gmail.com>
2023-06-28 14:57:53 -0500

    Clean up footer errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,6 @@
-import Link from "next/link";
 import Heading from "@/components/Heading";
 
 export default function Footer({ data, location = false }) {
-  const { main_cta } = data;
   return (
     <>
       <div className="lg:bg-[url('/images/pattern-footer.png')] bg-[position:center_75%] bg-[size:90%] bg-no-repeat bg-zinc-900 text-center text-white p-16 md:p-28 rounded-tl-sm md:rounded-tl-md rounded-tr-sm md:rounded-tr-md">
@@ -10,21 +8,7 @@ export default function Footer({ data, location = false }) {
           {data?.headline}
         </Heading>
         <p>{data?.description}</p>
-        {/* <Button href={main_cta?.href} className="mt-6">
-          {main_cta?.title}
-        </Button> */}
       </div>
-      {location && (
-        <div className="bg-teal text-center p-4 md:p-6">
-          <p>
-            Can&apos;t make it to this ElasticON location?{" "}
-            <Link className="underline" href="/">
-              Find a location
-            </Link>{" "}
-            closer to you.
-          </p>
-        </div>
-      )}
     </>
   );
 }
