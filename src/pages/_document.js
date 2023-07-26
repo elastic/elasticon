5d5df95550d2719e68de308f1e6f5ba317252c5f
John Barrier Wilson <johnbarrierwilson@gmail.com>
2023-07-26 18:43:18 -0500

    Update favicon reference

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/events/elasticon/favicon.ico" />
+      </Head>
       <body>
         <div className="max-w-[1440px] mx-auto px-1 sm:px-2 md:px-4">
           <Main />
