924f1b1eb260777825d23660428d43a5c79ecc2c
John Barrier Wilson <johnbarrierwilson@gmail.com>
2023-07-26 13:24:03 -0500

    update footer image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Heading from "@/components/Heading";
 export default function Footer({ data, location = false }) {
   return (
     <>
-      <div className="lg:bg-[url('/images/pattern-footer.png')] bg-[position:center_75%] bg-[size:90%] bg-no-repeat bg-zinc-900 text-center text-white p-16 md:p-28 rounded-tl-sm md:rounded-tl-md rounded-tr-sm md:rounded-tr-md">
+      <div className="lg:bg-[url('/images/pattern-footer.svg')] bg-[position:center_50%] bg-[size:90%] bg-no-repeat bg-zinc-900 text-center text-white p-16 md:p-28 rounded-tl-sm md:rounded-tl-md rounded-tr-sm md:rounded-tr-md">
         <Heading className="mb-6" size="h3">
           {data?.headline}
         </Heading>
