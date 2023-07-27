434745970f029ba1016c088a0c0e286b66d05ab4
John Barrier Wilson <johnbarrierwilson@gmail.com>
2023-07-27 13:42:39 -0500

    Add google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,11 +5,32 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/events/elasticon/favicon.ico" />
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KNJMG2M');
+          `,
+          }}
+        />
       </Head>
       <body>
         <div className="max-w-[1440px] mx-auto px-1 sm:px-2 md:px-4">
           <Main />
         </div>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KNJMG2M"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <NextScript />
       </body>
     </Html>
