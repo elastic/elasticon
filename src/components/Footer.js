085b386399e8b2265a66fa0713323cad74f9f52c
Casey Zumwalt <casey.zumwalt@elastic.co>
2023-11-13 13:48:35 -0600

    Static image importing, footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,20 +9,35 @@ export default function Footer({ data, eventEnded, globalData, location }) {
     <>
       <div className="relative bg-zinc-900 text-center text-white p-16 md:p-28 rounded-tl-sm md:rounded-tl-md lg:rounded-tl-lg rounded-tr-sm md:rounded-tr-md lg:rounded-tr-lg">
         <Image src={footerBg} alt="" className="hidden lg:block absolute top-1/2 left-1/2 w-[90%] h-auto transform -translate-x-1/2 -translate-y-1/2" />
-        <Heading className="mb-6" size="h3">
-          {eventEnded ? `Didn't make it to ${location}?` : `Can't make it to ${location}?`}
-        </Heading>
-        <p className={location && "mb-10"}>
-          {location
-            ? `Find an ${globalData.series_name} near you.`
-            : data?.description}
-        </p>
-        {location && (
+        <div className="relative z-10">
+          {location ? (
+            <>
+              <Heading className="mb-6" size="h3">
+                {eventEnded ? `Didn't make it to ${location}?` : `Can't make it to ${location}?`}
+              </Heading>
+              <p className={location && "mb-10"}>
+                {location
+                  ? `Find an ${globalData.series_name} near you.`
+                  : data?.description}
+              </p>
+            </>
+          ) : (
+            <>
+              <Heading className="mb-6" size="h3">
+                We're looking forward to seeing you!
+              </Heading>
+              <p className="mb-10">
+                Register now to take advantage of early bird pricing.
+              </p>
+            </>
+
+          )}
+
           <Button outlined href="/#locations">
             View all events
           </Button>
-        )}
-      </div>
+        </div>
+      </div >
     </>
   );
 }
