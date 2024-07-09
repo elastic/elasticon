d5f89d5a78d4da6f0654d75a488d4b6d2211d586
John Barrier Wilson <john.wilson@elastic.co>
2024-07-09 14:41:21 -0500

    Set locations tab based on URL param
    Affects https://github.com/elastic/website-development/issues/13373

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,19 @@ import Heading from "@/components/Heading";
 import footerBg from "@/images/pattern-footer.png";
 import Image from "next/image";
 
-export default function Footer({ data, eventEnded, globalData, location }) {
+export default function Footer({ data, eventEnded, globalData, locationData }) {
   return (
     <>
       <div className="relative bg-zinc-900 text-center text-white p-16 md:p-28 rounded-tl-sm md:rounded-tl-md lg:rounded-tl-lg rounded-tr-sm md:rounded-tr-md lg:rounded-tr-lg">
         <Image src={footerBg} alt="" className="hidden lg:block absolute top-1/2 left-1/2 w-[90%] h-auto transform -translate-x-1/2 -translate-y-1/2" />
         <div className="relative z-10">
-          {location ? (
+          {locationData ? (
             <>
               <Heading className="mb-6" size="h3">
-                {eventEnded ? `Didn't make it to ${location}?` : `Can't make it to ${location}?`}
+                {eventEnded ? `Didn't make it to ${locationData.title}?` : `Can't make it to ${locationData.title}?`}
               </Heading>
-              <p className={location && "mb-10"}>
-                {location
+              <p className={locationData.title && "mb-10"}>
+                {locationData.title
                   ? `Find an ${globalData.series_name} near you.`
                   : data?.description}
               </p>
@@ -27,7 +27,7 @@ export default function Footer({ data, eventEnded, globalData, location }) {
             </Heading>
           )}
 
-          <Button outlined href="/#locations">
+          <Button outlined href={locationData ? `/?region=${locationData.region}#locations` : "/#locations"}>
             View all events
           </Button>
         </div>
--- src/pages/[location]/index.js
+++ src/pages/[location]/index.js
@@ -209,7 +209,7 @@ export default function Location({
         data={footerData}
         eventEnded={eventEnded}
         globalData={globalData}
-        location={locationData.title}
+        locationData={locationData}
       />
     </>
   );
